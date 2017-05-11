3624dc91a289a9c0ca39a2ea323b7c1d1deb39e2
Tim Gabets <tim@gabets.ru>
2017-05-11 11:56:04 +0300

    Added LICENCE stuff

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(name='pythales',
+      version='0.01',
+      
+      description='python thales hsm simulator',
+      long_description=open('README').read(),
+      
+      classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Operating System :: OS Independent',
+        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        
+        'Topic :: Communications',
+        'Intended Audience :: Developers',
+      ],
+      
+      keywords='thales hsm',
+      
+      url='https://github.com/timgabets/pythales',
+      author='Tim Gabets',
+      author_email='tim@gabets.ru',
+      
+      license='LGPLv2',
+      packages=['pythales'],
+      install_requires=[],
+      zip_safe=True)
