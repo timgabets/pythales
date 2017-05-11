0d6e30db1deb28aaeb347dc15fca85233cd965bc
Tim Gabets <tim@gabets.ru>
2017-05-11 14:50:11 +0300

    pythales module

--- pythales/hsm.py
+++ pythales/hsm.py
@@ -7,13 +7,20 @@ import struct
 
 from tracetools.tracetools import trace
 
-class HSM():
-    def __init__(self, _port=None):
-        if _port:
-            self.port = _port
+class HSM:
+    def __init__(self, port=None, header=None):
+        
+        if port:
+            self.port = port
         else:
-            self._port = 1500
+            self.port = 1500
 
+        if header:
+            self.header = header
+        else:
+            self.header = ''
+
+    def run(self):
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.bind(('', self.port))   
@@ -23,33 +30,35 @@ class HSM():
             print('Error starting server: {}'.format(msg))
             sys.exit()
 
-
-    def run(self):
         while True:
             conn, addr = self.sock.accept()
             print ('Connected client: ' + addr[0] + ':' + str(addr[1]))
 
             while True:
                 try:
-                    data = conn.recv(4096)
-                    trace('<< {} bytes received: '.format(len(data)), data)
+                    request = conn.recv(4096)
+                    trace('<< {} bytes received: '.format(len(request)), request)
                         
-                    Len = struct.unpack_from("!H", data[:2])[0]
-                    
-                    if(Len != len(data) - 2):
-                        print("Invalid length {0} - {1}".format(Len, len(data) - 2))
-                        conn.close()
+                    Len = struct.unpack_from("!H", request[:2])[0]
+                    if(Len != len(request) - 2):
+                        print("Invalid length {0} - {1}".format(Len, len(request) - 2))
                         continue
-                                    
-                    conn.send(data)
-                    trace('>> {} bytes sent:'.format(len(data)), data)
+                    
+                    response = self.get_response(request)
+                    conn.send(response)
+                    trace('>> {} bytes sent:'.format(len(response)), response)
     
                 except KeyboardInterrupt:
                     print('Exit')
                     s.close()
                     sys.exit()
     
-    
+
+    def get_response(self, request):
+        response = b'00'
+        return struct.pack("!H", len(response) + len(self.header)) + self.header + response
+
+
 def show_help(name):
     """
     Show help and basic usage
@@ -57,24 +66,27 @@ def show_help(name):
     print('Usage: python3 {} [OPTIONS]... '.format(name))
     print('Thales HSM command simulator')
     print('  -p, --port=[PORT]\t\tTCP port to listen, 1500 by default')
+    print('  -h, --header=[HEADER]\t\tmessage header, empty by default')
 
 if __name__ == '__main__':
-    port = 1500
-    max_conn = 5
+    port = None
+    header = ''
 
-    optlist, args = getopt.getopt(sys.argv[1:], 'hp:', ['help', 'port='])
+    optlist, args = getopt.getopt(sys.argv[1:], 'h:p:', ['header=', 'port='])
     for opt, arg in optlist:
-        if opt in ('-h', '--help'):
-            show_help(sys.argv[0])
-            sys.exit()
+        if opt in ('-h', '--header'):
+            header = arg
         elif opt in ('-p', '--port'):
             try:
                 port = int(arg)
             except ValueError:
                 print('Invalid TCP port: {}'.format(arg))
                 sys.exit()
+        else:
+            show_help(sys.argv[0])
+            sys.exit()
 
-    hsm = HSM(port)
+    hsm = HSM(port, header)
     hsm.run()
 
 
--- pythales/tests.py
+++ pythales/tests.py
@@ -2,5 +2,23 @@
 
 import unittest
 
+from pythales.hsm import HSM
+
+class TestHSM(unittest.TestCase):
+    def setUp(self):
+        self.hsm = HSM()
+
+    def test_message_header(self):
+        self.hsm = HSM(header='IDDQD')
+        self.assertEqual(self.hsm.header, 'IDDQD')
+
+    """
+    NC 
+    def test_NC(self):
+        self.assertEqual(self.trxn.set_expected_action('approve'), True)
+        self.assertEqual(self.trxn.expected_response_action, 'APPROVE')
+    """
+
+
 if __name__ == '__main__':
     unittest.main()
