a1bec9b3644d7e7da883226533f291ac3aa8d6ca
Tim Gabets <tim@gabets.ru>
2017-05-12 15:24:55 +0300

    v0.24 Dummy response to DC command

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pythales',
-      version='0.23',
+      version='0.24',
       
       description='python thales hsm simulator',
       long_description=open('README').read(),
