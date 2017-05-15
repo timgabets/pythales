f265b3c691731cedf8c983fd55e79c4ccb228c0d
Tim Gabets <tim@gabets.ru>
2017-05-15 17:15:47 +0300

    v0.30 Fixed _get_visa_pvv() bug related to TSP data representation

--- pythales/tests.py
+++ pythales/tests.py
@@ -131,11 +131,11 @@ class TestHSM(unittest.TestCase):
     hsm._get_visa_pvv()
     """
     def test_get_visa_pvv(self):
-        self.assertEqual(self.hsm._get_visa_pvv(b'4761260000000134', b'1', b'1234', b'DEADDEADDEADDEADBEAFBEAFBEAFBEAF'), b'5934')
+        self.assertEqual(self.hsm._get_visa_pvv(b'4761260000000134', b'1', b'1234', b'DEADDEADDEADDEADBEAFBEAFBEAFBEAF'), b'8289')
 
     def test_get_visa_pvv_incorrect_key(self):
         with self.assertRaisesRegex(ValueError, 'Incorrect key length'):
-            self.assertEqual(self.hsm._get_visa_pvv(b'4761260000000134', b'1', b'1234', b'DEADDEADDEADDEADBEAFBEAFBEAF'), b'5934')
+            self.hsm._get_visa_pvv(b'4761260000000134', b'1', b'1234', b'DEADDEADDEADDEADBEAFBEAFBEAF')
 
 
 if __name__ == '__main__':
